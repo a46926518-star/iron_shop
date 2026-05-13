8b6241154cd858e4e37170aee28f6e9b16d4d5cc
a46926518-star <278016321+a46926518-star@users.noreply.github.com>
2026-05-13 17:14:36 +0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,45 +1,96 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Category, Product, Order, OrderItem, Profile, Feedback, CartItem
+from .models import (
+    Category, Product, Master, Profile,
+    Lead, Cart, CartItem, Order,
+    OrderItem, Feedback, Wishlist, Payment
+)
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
-
 class ProductSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='category.name', read_only=True)
+    category_name = serializers.ReadOnlyField(source='category.name')
 
     class Meta:
         model = Product
         fields = '__all__'
 
-
-class OrderItemSerializer(serializers.ModelSerializer):
+class MasterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItem
+        model = Master
         fields = '__all__'
 
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = '__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product'
+    )
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity']
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items', 'created_at']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.ReadOnlyField(source='product.name')
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'product_name', 'quantity', 'price']
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    master_name = serializers.ReadOnlyField(source='master.full_name')
 
     class Meta:
         model = Order
+        fields = [
+            'id', 'user', 'master', 'master_name', 'full_name',
+            'phone_number', 'address', 'status', 'total_price',
+            'kanban_order', 'items', 'created_at', 'updated_at'
+        ]
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
         fields = '__all__'
 
+class WishlistSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
 
-class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CartItem
-        fields = '__all__'
+        model = Wishlist
+        fields = ['id', 'user', 'product']
 
-
-class ProfileSerializer(serializers.ModelSerializer):
+class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Profile
+        model = Payment
         fields = '__all__'
 
 
@@ -48,18 +99,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        fields = ["username", "password", "email"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email']
-
-class FeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = '__all__'
+        return User.objects.create_user(**validated_data)
