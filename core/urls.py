18eb42bb5470b80f5f1e0ac4aabbd483e88fca70
a46926518-star <278016321+a46926518-star@users.noreply.github.com>
2026-05-13 17:12:32 +0500

    Refactor URL patterns and add kanban page

--- core/urls.py
+++ core/urls.py
@@ -13,22 +13,26 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from api import views as api_views
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs/', permanent=True)),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('api/', include('api.urls')),
+    path('kanban/', api_views.kanban_page, name='kanban-board'),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('accounts/', include('django.contrib.auth.urls')),
-
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
